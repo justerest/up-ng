5abacd2572428d7a314720394aa086929eb8b6cc
Sergey Klevakin <justerest@yandex.ru>
2018-11-17 19:06:13 +0500

    fix: helper can't change existing type

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -2,20 +2,22 @@ import { Observable, ObservableInput, OperatorFunction } from 'rxjs';
 export declare function init<T, K extends string>(key: K): OperatorFunction<T, {
     [P in K]: T;
 }>;
-export declare function set<T extends object, R, K extends string>(key: K extends keyof T ? never : K, fn: (scope: T) => ObservableInput<R>): (scope: T) => Observable<{
+export declare function set<T extends object, K extends keyof T>(key: K, fn: (scope: T) => ObservableInput<T[K]>): (scope: T) => Observable<T>;
+export declare function set<T extends object, K extends string, R>(key: K, fn: (scope: T) => ObservableInput<R>): (scope: T) => Observable<{
     [P in keyof (T & {
-        [k in K]: R;
+        [S in K]: R;
     })]: (T & {
-        [k in K]: R;
+        [S in K]: R;
     })[P];
 }>;
-export declare function add<T extends object, R, K extends string>(key: K extends keyof T ? never : K, fn: (scope: T) => R): (scope: T) => {
+export declare function add<T extends object, K extends keyof T>(key: K, fn: (scope: T) => T[K]): (scope: T) => T;
+export declare function add<T extends object, K extends string, R>(key: K, fn: (scope: T) => R): (scope: T) => {
     [P in keyof (T & {
-        [k in K]: R;
+        [S in K]: R;
     })]: (T & {
-        [k in K]: R;
+        [S in K]: R;
     })[P];
 };
-export declare function omit<T extends object, R>(fn: (scope: T) => ObservableInput<R>): (scope: T) => Observable<{
+export declare function omit<T extends object>(fn: (scope: T) => ObservableInput<any>): (scope: T) => Observable<{
     [P in keyof T]: T[P];
 }>;
--- src/utils.ts
+++ src/utils.ts
@@ -7,19 +7,21 @@ export function init(key: string) {
     return map((data) => ({ [key]: data }));
 }
 
-export function set<T extends object, R, K extends string>(key: K extends keyof T ? never : K, fn: (scope: T) => ObservableInput<R>): (scope: T) => Observable<{ [P in keyof (T & { [k in K]: R; })]: (T & { [k in K]: R; })[P]; }>;
+export function set<T extends object, K extends keyof T>(key: K, fn: (scope: T) => ObservableInput<T[K]>): (scope: T) => Observable<T>;
+export function set<T extends object, K extends string, R>(key: K, fn: (scope: T) => ObservableInput<R>): (scope: T) => Observable<{ [P in keyof (T & { [S in K]: R; })]: (T & { [S in K]: R; })[P]; }>;
 export function set(key: string, fn: any) {
     return (scope) => from(fn(scope)).pipe(
         map((response) => ({ ...scope, [key]: response })),
     );
 }
 
-export function add<T extends object, R, K extends string>(key: K extends keyof T ? never : K, fn: (scope: T) => R): (scope: T) => { [P in keyof (T & { [k in K]: R; })]: (T & { [k in K]: R; })[P]; };
+export function add<T extends object, K extends keyof T>(key: K, fn: (scope: T) => T[K]): (scope: T) => T;
+export function add<T extends object, K extends string, R>(key: K, fn: (scope: T) => R): (scope: T) => { [P in keyof (T & { [S in K]: R; })]: (T & { [S in K]: R; })[P]; };
 export function add(key: string, fn: any) {
     return (scope) => ({ ...scope, [key]: fn(scope) });
 }
 
-export function omit<T extends object, R>(fn: (scope: T) => ObservableInput<R>): (scope: T) => Observable<{ [P in keyof T]: T[P]; }>;
+export function omit<T extends object>(fn: (scope: T) => ObservableInput<any>): (scope: T) => Observable<{ [P in keyof T]: T[P]; }>;
 export function omit(fn: any) {
     return (scope) => from(fn(scope)).pipe(mapTo(scope));
 }
